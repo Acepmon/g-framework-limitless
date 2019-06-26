4dede61b1527b8f67867b4e5c2dfae45db0ae6cd
Tsogtbayar Davaadorj <acep.world@gmail.com>
2019-06-26 17:32:15 +0800

    initial commit

---
+++ includes/footer.blade.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<div class="footer text-muted text-center">
+    &copy; 2019. <a href="#">G-Framework</a> by <a href="http://gcommunication.mn" target="_blank">G Communication</a>
+</div>
+<!-- /footer -->
